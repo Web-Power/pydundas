b47bc7da8cbf9ac3447df2827a84c11a7ad95e64
Guillaume <guillaume.roger@webpower.nl>
2019-04-12 11:37:45 +0200

    meta define new apis

--- pydundas/rest/api.py
+++ pydundas/rest/api.py
@@ -1,12 +1,41 @@
+import importlib
+
+
 class Api:
 
     def __init__(self, session):
         self.session = session
-        self._project = None
+        self.define_all(['project'])
 
-    def project(self):
-        if not self._project:
-            from .project import Project
-            self._project = Project(self.session)
+    def define_all(self, calls):
+        """
+        After the 3rd copy/paste/replace I went meta.
+        All (or most at least) classes will have the same template:
+        Create a _apiname attribute holding the instance Apiname(self.session)
 
-        return self._project
+        This method gets an array of apinames to create that way, and the magic is done all dynamically.
+        In comments are the lines you would need to explicitly write for eg. notification().
+
+        """
+        for c in calls:
+
+            # This is the method that will be bound the proper attribute name.
+            def template():
+                # if not self._notification:
+                apimethod = '_ ' + c
+                if not getattr(self, apimethod, None):
+                    # from .notification import Notification
+
+                    # __module__ is pydundas.rest.api, and relative imports need the package name, which is everything
+                    # up to the last dot.
+                    pkg = self.__module__.rpartition('.')[0]
+                    mod = importlib.import_module('.' + c, pkg)
+                    cls = getattr(mod, c.capitalize())
+
+                    # self._notification = Notification(self.session)
+                    setattr(self, apimethod, cls(self.session))
+
+                # return self._notification
+                return getattr(self, apimethod)
+
+            setattr(self, c, template)
