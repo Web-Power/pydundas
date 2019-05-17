561c937f2c20e119e9aa05e5785ac433aad7c680
Guillaume <guillaume.roger@webpower.nl>
2019-05-17 16:04:53 +0200

    Adding runs management for notifications

--- pydundas/rest/notification.py
+++ pydundas/rest/notification.py
@@ -1,3 +1,5 @@
+import time
+
 class Notification:
 
     def __init__(self, session):
@@ -21,12 +23,70 @@ class Notification:
             "options": "None"
         }])
 
+    def getById(self, nid):
+        return self.session.get(f'notification/{nid}').json()
+
     def run(self, ids):
-        """Run all notifications with given ids. If Id is a string, just run the one."""
+        """Run all notifications with given ids. If Id is a string, just run the one. There is no output
+        on success."""
         if isinstance(ids, str):
             ids = [ids]
 
-        self.session.post('notification/run', **{'json': ids})
+        return self.session.post('notification/run', **{'json': ids})
+
+    def isRunning(self, nid):
+        """True if the notification in parameter is running, false otherwise."""
+        lastrun = self.getLastRun(nid)
+        if lastrun:
+            return lastrun['status'].lower() == 'running'.lower()
+        else:
+            return False
+
+    def waitForCompletedRun(self, nid):
+        """Wait for the notification in parameter to be complete."""
+        while self.isRunning(nid):
+            time.sleep(15)
+
+    def getLastRun(self, nid):
+        """Return the Last run of the notification given in ID."""
+        pageNumber = 1
+        pageSize = 25  # That's what Dundas uses as default.
+        while True:
+            batch = self.session.post('job/query', **{
+                'json': {
+                    "queryJobsOptions": {
+                        "pageNumber": pageNumber,
+                        "pageSize": pageSize,
+                        "orderBy": [{
+                            "jobQueryField": "LastRunTime",
+                            "sortDirection": "Descending"
+                        }],
+                        "filter": [{
+                            "field": "None",
+                            "operator": "And",
+                            "options": "None",
+                            "value": [{
+                                "field": "JobKind",
+                                "operator": "Equals",
+                                "value": "Notification",
+                                "options": "None"
+                            }, {
+                                "field": "Status",
+                                "operator": "Equals",
+                                "value": "Deleted",
+                                "options": "InvertOperator"}],
+                        }]
+                    }
+                }
+            }).json()
+            if batch:
+                for n in batch:
+                    if n['relatedItemId'] == nid:
+                        return n
+            else:
+                return None
+
+
 
     # Default filter: all
     def _getWithFilter(self, filters=[]):
