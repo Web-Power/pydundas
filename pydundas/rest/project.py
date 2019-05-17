6bd28b886f79b5735b08a1dddb3cbf4d619d7170
Guillaume <guillaume.roger@webpower.nl>
2019-05-17 13:23:30 +0200

    Get project by name or id

--- pydundas/rest/project.py
+++ pydundas/rest/project.py
@@ -7,10 +7,16 @@ class Project:
     def __init__(self, session):
         self.session = session
 
-    def getProjectIDByName(self, name):
+    def getProjectIdByName(self, name):
+        """Return a project Id based on its exact name."""
+        return self.getProjectByName(name)['projectId']
+
+    def getProjectByName(self, name):
+        """Returns the first project with the name given in parameter. The output will contain the whole project
+        definition."""
         ll = list(filter(lambda x: x['objectType'] == 'Project' and x['name'] == name, self.getAll()))
         if len(ll):
-            return ll[0]['projectId']
+            return ll[0]
         else:
             raise ProjectNotFound("No project named '{}'.".format(name))
 
